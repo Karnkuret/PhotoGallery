7045c02c6f0070c64beda667a3b4211ca667ebbf
Karnkuret <ricardo.avila.carrillo@gmail.com>
2017-01-04 19:05:15 -0800

    *Added activity_fragment.xml & SingleFragmentActivity.java to host a recyclerView fragment *Created the fragment PhotoGalleryFragment.java, it uses an AsyncTask to run in the background the connection to Flickr *FlickrFetchr.java connects to Flickr fetching raw data from a URL and returning it as an array of objects & converting the result to string. *GalleryItem.java is a model object class

---
+++ app/src/main/java/com/example/ricardo/photogallery/GalleryItem.java
@@ -0,0 +1,40 @@
+package com.example.ricardo.photogallery;
+
+/**
+ * Created by Ricardo on 1/4/2017.
+ */
+
+public class GalleryItem {
+    private String mCaption;
+    private String mId;
+    private String mUrl;
+
+    @Override
+    public String toString() {
+        return mCaption;
+    }
+
+    public String getCaption() {
+        return mCaption;
+    }
+
+    public void setCaption(String caption) {
+        mCaption = caption;
+    }
+
+    public String getId() {
+        return mId;
+    }
+
+    public void setId(String id) {
+        mId = id;
+    }
+
+    public String getUrl() {
+        return mUrl;
+    }
+
+    public void setUrl(String url) {
+        mUrl = url;
+    }
+}
